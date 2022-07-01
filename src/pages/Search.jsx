7fa2c7c68be130398a19ac4194873e53dd3319f3
João Mattedi <joaommattedi@gmail.com>
2022-07-01 17:53:53 -0300

    controlled search button

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      invalidAlbum: true,
+    };
+  }
+
+  inputChecker = (event) => {
+    const { value } = event.target;
+    if (value.length >= 2) {
+      return this.setState({ invalidAlbum: false });
+    }
+  }
+
   render() {
+    const { invalidAlbum } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.inputChecker }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ invalidAlbum }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
