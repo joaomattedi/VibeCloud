e448a940211ba93d4aa8da488146652bf677e159
João Mattedi <joaommattedi@gmail.com>
2022-07-01 17:39:12 -0300

    nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -23,6 +24,11 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <h3 data-testid="header-user-name">{name}</h3>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">SEARCH</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">FAVORITES</Link>
+          <Link to="/profile" data-testid="link-to-profile">PROFILE</Link>
+        </nav>
       </header>
     );
   }
