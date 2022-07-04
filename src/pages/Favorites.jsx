6d8970331d0594db65764ab3b58dffd744b85fd3
João Mattedi <joaommattedi@gmail.com>
2022-07-04 14:37:31 -0300

    favorites page implemented

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      musicsFav: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const musics = await getFavoriteSongs();
+      this.setState({ musicsFav: musics, loading: false });
+    });
+  }
+
+  updateFavorites = async () => {
+    const favorites = await getFavoriteSongs();
+    this.setState({ musicsFav: favorites });
+  }
+
   render() {
+    const { loading, musicsFav } = this.state;
+    if (loading) return <Loading />;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {musicsFav.map((element) => (<MusicCard
+          key={ element.trackId }
+          previewUrl={ element.previewUrl }
+          trackName={ element.trackName }
+          trackId={ element.trackId }
+          updateFavorites={ this.updateFavorites }
+          favorite={ musicsFav.some(({ trackId }) => trackId === element.trackId) }
+        />))}
       </div>
     );
   }
